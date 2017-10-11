b0f105019bc8982182d9906fc671a6e41d6cf25b
emcd123 <mcdonnell.owen1@gmail.com>
2017-10-11 11:37:44 +0100

    Formatted add game

--- TheGameList/db/seeds.rb
+++ TheGameList/db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: utf-8
+Game.delete_all
+
+Game.create!(title: 'Shovel Knight:Treasure Trove',
+	year: 2017,
+	developer: 'Yacht Club Games',
+	platform: 'Nintendo Switch',
+	genre: 'Platformer',
+	rating: 4.5,
+	description:
+		%{<p>
+				Shovel Knight: Treasure Trove is the full and complete
+				edition of Shovel Knight, a sweeping classic action adventure 				game series with awesome gameplay, memorable characters, and 					an 8-bit retro aesthetic! Become Shovel Knight, wielder of 					the Shovel Blade, as he runs, jumps, and battles in a quest 				for his lost beloved. Take down the nefarious knights of the 					Order of No Quarter and their menacing leader, The 		Enchantress.
+			</p>},	
+	box_art_url: 'shovelknight.jpg')
+	
+
+Game.create!(title: 'Legend of Zelda:Breath of the Wild',
+	year: 2017,
+	developer: 'Nintendo',
+	platform: 'Nintendo Switch',
+	genre: 'Adventure',
+	rating: 5.0,
+	description:
+		%{<p> Step into a world of adventure
+
+				Forget everything you know about The Legend of Zelda games. 				Step into a world of discovery, exploration and adventure in 					The Legend of Zelda: Breath of the Wild, a boundary-breaking 					new game in the acclaimed series. Travel across fields, 				through forests and to mountain peaks as you discover what 					has become of the ruined kingdom of Hyrule in this stunning 				open-air adventure. 
+			</p>},	
+	box_art_url: 'zeldabotw.jpg')
